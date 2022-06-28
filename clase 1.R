87e657b5b371aed733d4f5c2d5a5c8a035baaa1c
forlinolara <lara.rfw@gmail.com>
2022-06-28 20:48:16 -0300

    agrego archivos

---
+++ clase 1.R
@@ -0,0 +1,79 @@
+#Este paquete traduce lenguaje de R a git
+#install.packages("usethis")
+#library(usethis)
+#use_git_config(user.name = "Acá tu numbre", user.email = "acatumail@example.com")
+##Eso lo hago una vez por compu
+#user.name=forlinolara, email=lara.rfw@gmail.com
+
+#Comandos en terminal para conectar con github - esto lo hago cada vez que
+#quiero actualizar de git. Codigo:
+#git add -A (agrego los archivos nuevos - A es de ALL)
+#git commit -m "agrego nuevos archivos" - agrego comentarios
+#"git push" - para actualizar en gitbub
+
+#preguntas: creas un repositorio con el html de github, en donde se crea la carpeta local?
+#diferncia entre commit y add (commit es solo el comentario?)
+
+#Descargar paquetes oficiale - que pasaron por CRAN, no significa trucho - 
+#install.packages("nombre_del_paquete")
+
+#Para usar paquetes se pone library una vez por sesion
+#library 
+
+#Tidyverse: paquete de paquetes - ggplot, purr, dplyr, readr, tidyr, tibble
+library(tidyverse)
+#Dialectos para R: R base vs Tidyverse
+#Creo objeto, con el comando asignar <- le digo que el objeto es un dataframe compuesto por 5 variables o vectores (sinónimos). Primero vector de texto, luego numérico. Son como columnas. 
+curso <- data.frame(nombre     = c("Juan", "Pedro", "María", "José","Enzo", "Ariel", "Eva"),
+                    edad       = c(25, 32, 21,40, 30, 28, 37),
+                    nacimiento = c(1993, 1986, 1997, 1978, 1988, 1990, 1981),
+                    programa   = c("spss", "stata", "stata", "excel", "R", "stata", "spss"),
+                    nivel      = c(3, 5,7, 6, 2, 6, 8))
+
+#Si quiero calcular el promedio de edad con R base uso esto:
+  mean(curso$edad)
+
+#Con $ navego sobre las columnas de la base.
+
+#Con tidyverse
+
+curso$edad %>% 
+  mean() %>%
+  round(digits = 1)
+
+#Lo que dejo de hacer es especificar el mean sobre quien corre, ya esta a la izquierda del pipe. El pipe sirve para identificar que voy a hacer, en este caso usar mean. 
+#round es para redondear, digitos es un parametro de esa funcion. 
+
+#Con R base debería hacer esto para redondear
+round(mean(curso$edad), digits = 1)
+
+#Practico. Traducir esto a tidyverse
+round(prop.table(table(curso$programa)) * 100, digits = 1)
+
+###ATAJO PIPE CTL+SHIFT+M
+###LISTADO DE ATAJOS ALTL+SHIFT+K
+#Table: sirve para saber la frecuencia de una variable
+#prop.table: se hace sobre table y es para saber frecuencia relativa
+#round: para redondear
+
+curso$programa %>% 
+  table() %>% 
+  prop.table() %>% 
+  round(digits = 1)*100
+
+#Paquetes para  importar archivos
+#Paquete {readr}
+#•	read_csv() --> .csv
+#•	read_table() --> .txt
+#•	read_rds() --> .rds
+#Paquete {haven}
+#•	read_sav() --> SPSS
+#•	read_dta() --> STATA
+#•	read_sas() --> SAS
+
+library (readr)
+library(haven)
+
+#Descargo Base de datos: https://datos.gob.ar/dataset/otros-inmuebles-propios-estado-nacional-alquilados
+
+inmuebles <- read_csv("C:/Users/laraf/OneDrive/Documentos/MAESTRIA/Genero/utdt/inmuebles-estado-nacional.csv")
